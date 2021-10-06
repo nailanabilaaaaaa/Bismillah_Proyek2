04d11e9f902f6163e9e1a5e390f4ae94c39bc269
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2021-10-06 11:51:24 +0700

    Update View Admin

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
 <div class="container">
@@ -84,6 +84,7 @@
                         </div>
                     </form>
                 </div>
+                <a class="btn btn-success mt-3" href="/">Kembali</a>
             </div>
         </div>
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
 <div class="container">
@@ -23,7 +23,7 @@
                         <tr><th>Created At</th><th>:</th><td>{{ $user->created_at }}</td></tr>
                     </table>
                 </div>
-                <a class="btn btn-success mt-3" href="Beranda">Beranda</a>
+                <a class="btn btn-success mt-3" href="DataCRUD">Masuk ke Data</a>
             </div>
         </div>
     </div>
